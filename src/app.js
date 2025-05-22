c2d005acb95e0cbf5bbf4919c514ed76abb2c09c
akalya007 <akalya.d2022eee@sece.ac.in>
2025-05-22 07:43:26 +0530

    get API

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.post("/signup" , async(req, res)=>{
     // const userObj = {
     //  firstName : "Akalya",
     //  lastName: "Dhanapal",
-    //  emailID: "akalyadhanapal.com",
+    //  emailId: "akalyadhanapal.com",
     //  password: "akalya@123"
     // }
      //to save the User into the mongodb database--need to create the new instance for the model.
@@ -18,7 +18,6 @@ app.post("/signup" , async(req, res)=>{
     //another way
       
     const user = new User(req.body);
-
     try{
       await user.save();   //most of the mongoose function returns promise. so use async and await.
       res.send("User added successfully");
@@ -27,6 +26,31 @@ app.post("/signup" , async(req, res)=>{
     }
   
 });
+
+//to get the user by their email.
+app.get("/getuser" , async(req, res)=>{
+    const userEmail=req.body.emailId;
+    try{
+    const users=await User.find({emailId : userEmail});
+    if(users.length === 0){
+        res.status(404).send("User not found");
+    }else{
+   res.send(users);
+    }
+    }catch(err){
+        res.send(400).send("something went wrong");
+    }
+});
+
+//get all the user form the database
+app.get("/feed" , async (req, res) =>{
+    try{
+      const users = await User.find({});
+      res.send(users);
+    }catch{
+        res.status(400).send("something went wrong");
+    }
+})
 
 connectDB()       //hence it is the promise, we were handling it.
 .then(()=>{
