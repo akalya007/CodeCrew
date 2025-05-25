e1233c9e31722e04f2d3638c20b639a7b085799f
akalya007 <akalya.d2022eee@sece.ac.in>
2025-05-25 20:22:23 +0530

    UserAuth middleware

--- src/app_learning2.js
+++ src/app_learning2.js
@@ -1,6 +1,4 @@
 
-//Building the proper API--for CodeCrew.
-
 const express=require("express");
 const app = express();
 const connectDB = require("./config/database");   //for connecting the database.
@@ -71,11 +69,11 @@ const isPasswordValid = await bcrypt.compare(password , user.password);
 if(isPasswordValid){    //If it  is valid only, It will generate the token.
   
 //create JWT token
- const token = await jwt.sign({_id: user._id} , "codecrew@123" , { expiresIn: "1d"})   // jwt.sign({keep data that need to hidden} ,"secret key")--here we hiding the userId
+ const token = await jwt.sign({_id: user._id} , "codecrew@123")   // jwt.sign({keep data that need to hidden} ,"secret key")--here we hiding the userId
  console.log(token);   //this token is also has the secret info about whuo is loged in . 
 
   //Add the token to the cookie abd sebd the response back to the user.
-res.cookie("token" , token , {expires: new Date(Date.now() + 8*36000000 )});    //cookie is given by express.
+res.cookie("token" , token);    //cookie is given by express.
 res.send("Login Successfully");
 }else{
     throw new Error("Invalid credentails")
@@ -85,6 +83,32 @@ res.send("Login Successfully");
     }
 }) 
 
+
+//to get the user by their email.
+app.get("/getuser" , async(req, res)=>{
+        //first read from the request
+    const userEmail=req.body.emailId;
+    try{
+    const users=await User.find({emailId : userEmail});
+    if(users.length == 0){
+        res.status(404).send("User not found");
+    }else{
+   res.send(users);
+    }
+    }catch(err){
+        res.send(400).send("something went wrong");
+    }
+});
+
+//get all the user form the database
+app.get("/feed" , async (req, res) =>{
+    try{
+      const users = await User.find({});
+      res.send(users);
+    }catch{
+        res.status(400).send("something went wrong");
+    }
+})
 
 app.get("/profile", UserAuth, async(req,res)=>{
     try{
@@ -100,15 +124,70 @@ app.get("/profile", UserAuth, async(req,res)=>{
 })
 
 
-app.post("/sentConnectionRequest" , UserAuth , (req , res) =>{
-const user = req.user;
-
-    console.log("Connection request has sent");
-    res.send( user.firstName +" sent a conntion request");
+//delete the userby ID
+app.delete("/user" , async(req ,res)=>{
+    const userId = req.body.userId; //first read from the request
+    try{
+        //const user = await User.findByIdAndDelete({ _id:userId}) 
+     const user = await User.findByIdAndDelete(userId)   //it is te shorthand for ({_id : id})
+    res.send("user Deleted successfully");
+    }
+   catch{
+        res.status(400).send("something went wrong");
+    }
 })
 
+//update the data of the user
+app.patch("/user/:userId" , async(req , res)=>{    //it should not allow to update the random.
+    //first read from the request
+    const userId = req.params?.userId;
+    const data=req.body;   //data contains everything inoside the body-
+    console.log(data)
 
+    try{
+    const ALLOW_UPDATED =[         //checking each and every data is allowed to update or not.
+        "userId","photoUrl" , "about" , "gender" , "age", "skills"];   //we should not allow every data to be changed.
 
+     const isUpdateAllowed = Object.keys(data).every((k) =>       
+            ALLOW_UPDATED.includes(k));
+     if(!isUpdateAllowed){
+                throw new Error("update not allowed ");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
+     const user= await User.findByIdAndUpdate({_id: userId} , data , {
+        returnDocument : "before",
+        runValidators: true,
+    });   //it will ignore the userId in the data, because userId is not present in the schema.apart from the schema, it will be ignore.
+   
+    if(user.length ==0){
+        res.status(404).send("User not found");
+    }else{
+       res.send("User updated successfully")
+    }
+    }catch(err){
+        res.status(400).send("UPDATED FAILED : " + err.message);
+    }
+});
+
+//updating the data by the reference(email).
+app.patch("/userbyemail" ,async (req,res)=>{
+    //first read the data.
+    const email=req.body.emailId;
+    const data=req.body;
+   try{
+       const user = await User.findOneAndUpdate({emailId:email} ,data);
+   
+    if(user.length ==0){
+        res.status(404).send("User not found");
+    }else{
+       res.send("User updated successfully")
+    }
+    }catch{
+        res.status(400).send("something went wrong");
+    }
+})
 
 connectDB()       //hence it is the promise, we were handling it.
 .then(()=>{
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,26 +1,58 @@
-const adminAuth = (req , res , next)=>{
-    console.log("Admin auth is getting checked");
-    const token ="xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).res.send("Authorized request");
-    }else{
-        next();
-    }
-};
+// const adminAuth = (req , res , next)=>{
+//     console.log("Admin auth is getting checked");
+//     const token ="xyz";
+//     const isAdminAuthorized = token === "xyz";
+//     if(!isAdminAuthorized){
+//         res.status(401).res.send("Authorized request");
+//     }else{
+//         next();
+//     }
+// };
 
-const UserAuth = (req , res , next)=>{
-    console.log("User auth is getting checked");
-    const token ="xyz";
-    const isAdminAuthorized = token === "xyz";
-    if(!isAdminAuthorized){
-        res.status(401).res.send("Authorized request");
-    }else{
-        next();
-    }
-};
+// const UserAuth = (req , res , next)=>{
+//     console.log("User auth is getting checked");
+//     const token ="xyz";
+//     const isAdminAuthorized = token === "xyz";
+//     if(!isAdminAuthorized){
+//         res.status(401).res.send("Authorized request");
+//     }else{
+//         next();
+//     }
+// };
+
+
+
+const jwt = require("jsonwebtoken");
+const User=require("../model/user");
+
+
+//we have to give the authorization for every api that has been called,
+const UserAuth =async  (req , res , next)=>{
+ try{
+//1.read the token from the cookies
+const {token} = req.cookies;    //extracting the token from the cookies
+if(!token){
+    throw new Error("Token is not valid!!!!!!!")
+}
+
+//2.validate the token 
+const decodeObj = jwt.verify(token, "codecrew@123");
+
+const {_id} = decodeObj;
+
+  //3.find the user 
+  const user = await User.findById(_id);
+  if(!user){
+    throw new Error("User not found");
+  }
+  req.user = user;
+  next();    //if the token is valid, then the user is found , we just called next() 
+}
+catch(err){
+    res.status(400).send("ERROR: " + err.message);
+}  
+}
 
 module.exports={
-    adminAuth,
     UserAuth
 };
