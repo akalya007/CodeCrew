6a4e8c4c159f370f4d8635e16e34baeda4c40766
akalya_d <129743180+akalya007@users.noreply.github.com>
2026-01-28 15:14:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -119,7 +119,8 @@ const port = process.env.PORT || 3000;
 console.log("Port set to:", port);  // for debugging
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+        origin:"https://codecrew-web.onrender.com",
     credentials:true,
 }));
 app.use(express.json());
